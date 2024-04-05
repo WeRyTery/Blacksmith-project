6ccf29dc34721d1b422dada74afda34a88df02cc
VoVa228822 <146162374+VoVa228822@users.noreply.github.com>
2024-04-05 18:32:14 +0100

    ExportFilesTo3d

---
+++ blacksmith-project/Assets/ExportFolder/E_OrderingFoldler/E_OrdersDescription.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class E_OrdersDescription : MonoBehaviour
+{
+    public string material = "smt";
+    public string weaponType = "1";
+    public int budget = 123;
+}
