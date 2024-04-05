e2ed4d20b81f6f2a7e7413f62e4efbaa0172d594
VoVa228822 <146162374+VoVa228822@users.noreply.github.com>
2024-04-05 18:21:20 +0100

    new project 3d

--- blacksmith-project/Assets/VovasFolder/V_Scripts/MainUIManagement.cs
+++ blacksmith-project/Assets/VovasFolder/V_Scripts/MainUIManagement.cs
@@ -5,33 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainUIManagement : MonoBehaviour
 {
-    public GameObject buttonsPanel;
-    public GameObject settingsPanel;
-    public void PlayGame()
+    [SerializeField] private GameObject Settings;
+    [SerializeField] private GameObject[] SettingsPanels;
+
+    public void PlayGame(int index)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(index);
     }
     public void ShowSettings()
     {
-        buttonsPanel.SetActive(!buttonsPanel.active);
-        settingsPanel.SetActive(!settingsPanel.active);
-    }
-    public void CloseSettingsPanel()
-    {
-        if (settingsPanel != null)
-        {
-            settingsPanel.SetActive(false);
-
-            if (buttonsPanel != null)
-            {
-                buttonsPanel.SetActive(true);
-            }
-        }
+        Settings.SetActive(!Settings.active);
     }
     public void Exit()
     {
         Debug.Log("Exit");
         Application.Quit();
     }
-
+    public void OpenAudioSettings()
+    {
+        SettingsPanels[0].SetActive(!SettingsPanels[0].active);
+    } 
+    public void OpenGraphicSettings()
+    {
+        SettingsPanels[1].SetActive(!SettingsPanels[1].active);
+    } 
+    public void OpenKEyBindSettings()
+    {
+        SettingsPanels[2].SetActive(!SettingsPanels[2].active);
+    }
 }
