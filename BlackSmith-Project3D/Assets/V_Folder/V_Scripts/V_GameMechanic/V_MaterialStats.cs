f3684c3adc53bd3f689ff3c23f2f89b510f17b16
VoVa228822 <146162374+VoVa228822@users.noreply.github.com>
2024-06-18 17:25:34 +0100

    BetaVersion Game Mechanic Loop

---
+++ BlackSmith-Project3D/Assets/V_Folder/V_Scripts/V_GameMechanic/V_MaterialStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class V_MaterialStats : MonoBehaviour
+{
+    public int strength;
+    public int flexibility;
+}
