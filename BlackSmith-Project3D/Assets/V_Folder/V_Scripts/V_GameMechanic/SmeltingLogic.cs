38b0c64c1876ca13501ca37c48bcb0f69485dbce
VoVa228822 <146162374+VoVa228822@users.noreply.github.com>
2024-06-20 17:45:36 +0100

    SmeltingBase

--- BlackSmith-Project3D/Assets/V_Folder/V_Scripts/V_GameMechanic/SmeltingLogic.cs
+++ BlackSmith-Project3D/Assets/V_Folder/V_Scripts/V_GameMechanic/SmeltingLogic.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class SmeltingLogic : MonoBehaviour
 {
-    [SerializeField] private float TimeOfWaiting;
-    [SerializeField] private GameObject FirstModel; 
-    [SerializeField] private GameObject ButtonNext; 
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private Button Put_StartSmelting;
+    [SerializeField] private float TimeToSmeltMaterial;
+    [SerializeField] private bool PutMaterialIn = false;
+    [SerializeField] private GameObject SwordObject;
+    [SerializeField] private GameObject MaterialObject;
+    [SerializeField] private GameObject MeltedMaterialObject;
+
+
+    public void StartEverything()
     {
-        ButtonNext.SetActive(false);
-        StartCoroutine("SmeltingTime");
-        other.gameObject.SetActive(false);
+        if (PutMaterialIn)
+        {
+            MaterialObject.SetActive(false);
+            MeltedMaterialObject.SetActive(true);
+            StartCoroutine("StartSmelting");
+        }
+        else
+        {
+            PutMaterialIn = true;
+        }
     }
-    IEnumerator SmeltingTime()
+    IEnumerator StartSmelitng()
     {
-        yield return new WaitForSeconds(TimeOfWaiting);
-        FirstModel.SetActive(true);
-        ButtonNext.SetActive(true);
+        yield return new WaitForSeconds(TimeToSmeltMaterial);
+        MeltedMaterialObject.SetActive(false);
+        SwordObject.SetActive(true);
 
     }
 }
