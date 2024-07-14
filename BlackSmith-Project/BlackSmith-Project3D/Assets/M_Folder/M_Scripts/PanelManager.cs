7f376dbba79592a2cecbf295ec0e81d34dbe852b
Arhellus <mishamk3033@gmail.com>
2024-07-14 18:02:28 +0300

    GuideBook text

--- BlackSmith-Project/BlackSmith-Project3D/Assets/M_Folder/M_Scripts/PanelManager.cs
+++ BlackSmith-Project/BlackSmith-Project3D/Assets/M_Folder/M_Scripts/PanelManager.cs
@@ -8,6 +8,8 @@ public class PanelManager : MonoBehaviour
     public GameObject panel1;
     public GameObject panel2;
     public GameObject panel3;
+    public GameObject panel4;  // Новая панель 4
+    public GameObject panel5;  // Новая панель 5
 
     // Метод для открытия панели 1
     public void OpenPanel1()
@@ -15,6 +17,8 @@ public class PanelManager : MonoBehaviour
         panel1.SetActive(true);
         panel2.SetActive(false);
         panel3.SetActive(false);
+        panel4.SetActive(false);
+        panel5.SetActive(false);
     }
 
     // Метод для открытия панели 2
@@ -23,6 +27,8 @@ public class PanelManager : MonoBehaviour
         panel1.SetActive(false);
         panel2.SetActive(true);
         panel3.SetActive(false);
+        panel4.SetActive(false);
+        panel5.SetActive(false);
     }
 
     // Метод для открытия панели 3
@@ -31,6 +37,28 @@ public class PanelManager : MonoBehaviour
         panel1.SetActive(false);
         panel2.SetActive(false);
         panel3.SetActive(true);
+        panel4.SetActive(false);
+        panel5.SetActive(false);
+    }
+
+    // Метод для открытия панели 4
+    public void OpenPanel4()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(false);
+        panel3.SetActive(false);
+        panel4.SetActive(true);
+        panel5.SetActive(false);
+    }
+
+    // Метод для открытия панели 5
+    public void OpenPanel5()
+    {
+        panel1.SetActive(false);
+        panel2.SetActive(false);
+        panel3.SetActive(false);
+        panel4.SetActive(false);
+        panel5.SetActive(true);
     }
 }
 
