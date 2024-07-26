c1912df89eca45baa7356a1bcab189c424e26f48
VoVa228822 <146162374+VoVa228822@users.noreply.github.com>
2024-07-26 20:31:55 +0100

    Smithing Mechanic Half done

--- BlackSmith-Project3D/Assets/V_Folder/V_Scripts/V_GameMechanic/SmeltingLogic.cs
+++ BlackSmith-Project3D/Assets/V_Folder/V_Scripts/V_GameMechanic/SmeltingLogic.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 public class SmeltingLogic : MonoBehaviour
 {
     [SerializeField] GameObject FurnaceLights;
+    
+    Renderer renderer;
     [SerializeField] private float TimeToSmeltMaterial = 5f;
+
     public SmithingCycle ControlOfMechanic;
 
     public void SmeltingStart()
@@ -18,17 +21,16 @@ public class SmeltingLogic : MonoBehaviour
 
     IEnumerator StartSmelting()
     {
-        ControlOfMechanic.Materials[ControlOfMechanic.MaterialIndex].SetActive(true);
+        ControlOfMechanic.MaterialGameObject.SetActive(true);
+        renderer = ControlOfMechanic.MaterialGameObject.GetComponent<Renderer>();
+        renderer.material = ControlOfMechanic.MaterialsC[ControlOfMechanic.MaterialIndex];
 
         yield return new WaitForSeconds(TimeToSmeltMaterial);
 
         FurnaceLights.SetActive(false);
-        ControlOfMechanic.Materials[ControlOfMechanic.MaterialIndex].SetActive(false);
-        ControlOfMechanic.Instruments[0].SetActive(true);
-
-
-        ControlOfMechanic.Materials[ControlOfMechanic.MaterialIndex].SetActive(false);
-
+        ControlOfMechanic.MaterialGameObject.SetActive(false);
+        renderer = ControlOfMechanic.Instruments[0].GetComponent<Renderer>();
+        renderer.material = ControlOfMechanic.MaterialsC[ControlOfMechanic.MaterialIndex];
         ControlOfMechanic.Instruments[0].SetActive(true);
         ControlOfMechanic.Instruments[0].transform.position = ControlOfMechanic.Positions[0].transform.position;
         Debug.Log("Smelting");
